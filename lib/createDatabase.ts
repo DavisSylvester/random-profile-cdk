52625e052a05f42891ed36fcfe54dee68b0e2db2
Davis Sylvester <davis@sylvesterllc.com>
2022-09-21 07:13:09 -0500

    Add GSI

--- lib/createDatabase.ts
+++ lib/createDatabase.ts
@@ -1,4 +1,4 @@
-import { SecretValue } from "aws-cdk-lib";
+import { CfnOutput, SecretValue } from "aws-cdk-lib";
 import { InstanceClass, InstanceSize, InstanceType, IVpc, SecurityGroup, SecurityGroupProps, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
 import { Credentials, DatabaseInstance, DatabaseInstanceEngine, DatabaseInstanceProps, PostgresEngineVersion } from "aws-cdk-lib/aws-rds";
 import { Construct } from "constructs";
@@ -7,7 +7,9 @@ import { appConfig } from "../config/app";
 export const createDatabase = (scope: Construct, vpc: IVpc) => {
 
     const props = createCommonDatabaseProp(vpc)
-    new DatabaseInstance(scope, 'rnd-db', props);
+    const dbInstance = new DatabaseInstance(scope, 'rnd-db', props);
+
+    generateOutput(scope, dbInstance);
 };
 
 const createCommonDatabaseProp = (vpc: IVpc) => {
@@ -23,7 +25,7 @@ const createCommonDatabaseProp = (vpc: IVpc) => {
             SecretValue.unsafePlainText(appConfig.DATABASE.password!)),
         vpc,
         vpcSubnets: {
-          subnetType: SubnetType.PRIVATE_ISOLATED
+          subnetType: SubnetType.PUBLIC
         },
         securityGroups: [
           
@@ -35,6 +37,14 @@ const createCommonDatabaseProp = (vpc: IVpc) => {
 
 };
 
+
+const generateOutput = (scope: Construct, db: DatabaseInstance) => {
+
+    new CfnOutput(scope, 'database-output', {
+        exportName: 'databaseUrl',
+        value: db.dbInstanceEndpointAddress
+    });
+};
 
 // const createSecurityGroup = () => {
 
